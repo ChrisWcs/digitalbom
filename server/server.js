0514a8f7b86a63733969109d6be70cfe2d35c0aa
Christian <ChrisWcs@users.noreply.github.com>
2017-11-21 13:47:38 -0700

    Little tests with server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const test = require('./test');
+const app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+app.get('/1', (req, res) => res.send(`${test.text1}`));
+app.get('/2', (req, res) => res.send(`${test.text2}`));
