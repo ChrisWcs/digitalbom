c6cf827ef2ade6ca8ffa64c3c469cff16412abfa
Christian <ChrisWcs@users.noreply.github.com>
2017-12-01 12:22:54 -0700

    Modify json to work with routing a little better

--- server/server.js
+++ server/server.js
@@ -7,15 +7,18 @@ app.use(cors());
 
 app.listen(3000, () => console.log('Server running onport 3000'));
 
-app.get( '/verse', (req, res) => {
-    const { book, chapter, verse} = req.headers;
+app.get( '/book/:book/chapter/:chapter/verse/:verse', (req, res) => {
+    const { book, chapter, verse} = req.params;
     res.status(200).json({
         verse: bom[book][chapter][verse]
     });
 });
 
-app.get( '/chapter', (req, res) => {
-    const { book, chapter } = req.headers;
+app.get( '/book/:book/chapter/:chapter', (req, res) => {
+    console.log(req.params);
+    const { book, chapter } = req.params;
+    console.log(book);
+    console.log(chapter);
     res.status(200).json({
         chapter: bom[book][chapter]
     });
